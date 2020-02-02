37165b5ec97518e70c52ff9f17540f880e1af90b
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-02-02 20:42:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,13 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
   
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    
+    redirect to "/recipes/#{@recipe.id}"
+  end
+  
+
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
